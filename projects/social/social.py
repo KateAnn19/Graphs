bf3b8f9d784ee5be41941ade35c7f70f9d1f4bd1
kate mcgee <mcgeehee@gmail.com>
2020-10-08 12:24:33 -0500

    finish social network

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,5 +1,6 @@
 import random
 from util import Queue, Stack
+from math import *
 
 class User:
     def __init__(self, name):
@@ -9,10 +10,10 @@ class SocialGraph:
     def __init__(self):
         self.last_id = 0
         #maps IDs to User Objects
-        self.users = {}
+        self.users = {} # {1: User("1"), User("2"), ...}
         #Adjaceny List
         #Maps user_ids to a list of other users (who are their friends)
-        self.friendships = {}
+        self.friendships = {} # {1: {2,3,4}, 2: {1}, 3: {1}, 4: {1} }
 
     def add_friendship(self, user_id, friend_id):
         """
@@ -31,9 +32,9 @@ class SocialGraph:
         Create a new user with a sequential integer ID
         """
         self.last_id += 1  # automatically increment the ID to assign the new user
-        self.users[self.last_id] = User(name)
+        self.users[self.last_id] = User(name) # {1 : User("mari")}
         #adds vertex to adjacency list
-        self.friendships[self.last_id] = set()
+        self.friendships[self.last_id] = set() # {1: ()}
 
     def populate_graph(self, num_users, avg_friendships):
         """
@@ -52,12 +53,14 @@ class SocialGraph:
         for i in range(0, num_users):
             self.add_user(f"User {i}")
         # Create friendships
-        # Generate All possible friendships 
-        # Avoid duplicate friendships
+        # Generate All possible friendships and put into array
+        # 3 users (0, 1, 2)
+        # [(0, 1), (0,2), (1,2)]
+        
         possible_friendships = []
         for user_id in self.users:
+            # To prevent duplicate friendships create from user_id + 1
             for friend_id in range(user_id + 1, self.last_id + 1):
-                # user_id == user_id2 cannot happen
                 # if friendship between user_id and user_id_2 already exists
                     # don't add friendship between user_id_2 and user_id
                 possible_friendships.append((user_id, friend_id))
@@ -72,48 +75,49 @@ class SocialGraph:
             
 
     def get_all_social_paths(self, user_id):
-        """
-        Takes a user's user_id as an argument
-        Returns a dictionary containing every user in that user's
-        extended network with the shortest friendship path between them.
-        The key is the friend's ID and the value is the path.
-        """
+        pass
+    #     """
+    #     Takes a user's user_id as an argument
+    #     Returns a dictionary containing every user in that user's
+    #     extended network with the shortest friendship path between them.
+    #     The key is the friend's ID and the value is the path.
+    #     """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-          # create a queue
-        queue = []
-        # add the starting vertex to the queue initialized as a list
-        queue.append(self.friendships[user_id])
-        print(queue)
-        # create a set to store visited values in it to avoide infinite loops
-        # while vertices haven't been visited, loop (while the queue isn't empty)
-        while len(queue) > 0:
-            # pop off the first element in the list
-            path = queue.pop(0)
-            # grab the last element in the path 
-            current_node = path[-1]
-            # if the node isn't in visited, add it
-            if current_node not in visited:
-                visited.add(current_node)
-                # if the node is the destination then return the path
-                if current_node == destination_vertex:
-                    return path
-            # it is not the destination so grab that node's neighbors 
-            neigh = self.get_neighbors(current_node)
-            # loop through the node's neighbors since these are a set 
-            for n in neigh:
-                new = list(path)
-                new.append(n)
-                queue.append(new)
+    #     # !!!! IMPLEMENT ME
+    #       # create a queue
+    #     queue = []
+    #     # add the starting vertex to the queue initialized as a list
+    #     queue.append(self.friendships[user_id])
+    #     print(queue)
+    #     # create a set to store visited values in it to avoide infinite loops
+    #     # while vertices haven't been visited, loop (while the queue isn't empty)
+    #     while len(queue) > 0:
+    #         # pop off the first element in the list
+    #         path = queue.pop(0)
+    #         # grab the last element in the path 
+    #         current_node = path[-1]
+    #         # if the node isn't in visited, add it
+    #         if current_node not in visited:
+    #             visited.add(current_node)
+    #             # if the node is the destination then return the path
+    #             if current_node == destination_vertex:
+    #                 return path
+    #         # it is not the destination so grab that node's neighbors 
+    #         neigh = self.get_neighbors(current_node)
+    #         # loop through the node's neighbors since these are a set 
+    #         for n in neigh:
+    #             new = list(path)
+    #             new.append(n)
+    #             queue.append(new)
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    # sg.add_user("Artem")
-    # sg.add_user("Alice")
-    # sg.add_user("bob")
+    sg.add_user("Artem")
+    sg.add_user("Alice")
+    sg.add_user("bob")
     sg.populate_graph(5, 2)
     print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+    #connections = sg.get_all_social_paths(1)
+    #print(connections)
